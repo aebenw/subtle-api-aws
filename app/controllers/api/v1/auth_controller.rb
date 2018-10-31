7ecf1feca56667b85c306dbd24105dd35636dc6a
github username <github email address>
2018-10-31 17:24:26 -0400

    login reaches auth.create with crypted pass

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,8 +1,23 @@
-class AuthController < ApplicationController
+module Api
+  module V1
 
-  def create
+    class AuthController < ApplicationController
 
+      def create
+        user = User.find_by(email: user_params[:email])
+        if user && user.authenticate(user_params[:password])
+          render json: user
+        else
+          render json: {error: "User is invalid"}, status: 401
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:auth).require(:user).permit(:email, :password)
+      end
+
+    end
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/auth', to: 'auth#create'
+      post '/auth', to: 'auth#create'
       resources :blocks
       resources :channels
       resources :users
     end
-  end 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
