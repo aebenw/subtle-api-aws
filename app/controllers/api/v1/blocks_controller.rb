c5638cc56e0c6ad511670877e53850d7a6561076
github username <github email address>
2018-11-05 13:13:03 -0500

    create channel, some serializer

--- app/controllers/api/v1/blocks_controller.rb
+++ app/controllers/api/v1/blocks_controller.rb
@@ -1,9 +1,12 @@
 module Api
   module V1
-
     class BlocksController < ApplicationController
 
-    end
+      def index
+        blocks = Block.all
+        render json: blocks
+      end
 
+    end
   end
 end
--- app/serializers/channel_serializer.rb
+++ app/serializers/channel_serializer.rb
@@ -2,4 +2,11 @@ class ChannelSerializer < ActiveModel::Serializer
   attributes :id, :name, :users, :blocks
   has_many :users, through: :user_channel
   has_many :blocks, through: :channel_block
+
+  def blockss
+    object.blocks.map{ |x| x.user.name }
+  end
+
 end
+
+# ActiveModelSerializers::Adapter::Json.new(BlockSerializer.new(x))
