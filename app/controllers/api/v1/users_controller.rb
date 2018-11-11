e0717df8e7b8d6e5523c7313cfff10d5f1c67947
github username <github email address>
2018-11-10 23:40:51 -0500

    file name of blob to show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -38,25 +38,29 @@ module Api
 
       def content
 
-        num = [0,1,2].sample
-        content = []
-        type = ''
-        if num == 0
-          10.times do
-             content << UserSerializer.new(User.all.sample)
-           end
-          type = 'users'
-        elsif num == 1
-          10.times do
-            content << ChannelSerializer.new(Channel.all.sample)
-          end
-          type = 'channels'
-        elsif num == 2
-           10.times do
-             content << BlockSerializer.new(Block.all.sample)
-           end
-          type = 'blocks'
-        end
+        # num = [0,1,2].sample
+        # content = []
+        # type = ''
+        # if num == 0
+        #   10.times do
+        #      content << UserSerializer.new(User.all.sample)
+        #    end
+        #   type = 'users'
+        # elsif num == 1
+        #   10.times do
+        #     content << ChannelSerializer.new(Channel.all.sample)
+        #   end
+        #   type = 'channels'
+        # elsif num == 2
+        #    10.times do
+        #      content << BlockSerializer.new(Block.all.sample)
+        #    end
+        #   type = 'blocks'
+        # end
+        Block.first
+        block = [Block.first, Block.find(2)]
+        content = block.map{|x| BlockSerializer.new(x) }
+        type = "block"
 
         render json: {content: content, type: type}
 
